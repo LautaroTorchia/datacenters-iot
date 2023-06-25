b022e45abb2639602238077b190934a1fedf8056
Lautaro Torchia <torchia.lautaro2@gmail.com>
2023-06-25 20:51:04 -0300

    added api compose file

--- src/api-guardias/app.py
+++ src/api-guardias/app.py
@@ -4,31 +4,46 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+file_path = 'personas_guardia.json'
+
 # Verificar si existe un archivo de datos guardados
-if os.path.isfile('personas_guardia.json'):
-    with open('personas_guardia.json') as file:
-        personas_guardia = json.load(file)
+if os.path.isfile(file_path):
+    try:
+        with open(file_path) as file:
+            personas_guardia = json.load(file)
+    except (IOError, json.JSONDecodeError) as e:
+        print(f'Error al cargar el archivo JSON: {e}')
+        personas_guardia = {"guardias": []}
 else:
     personas_guardia = {"guardias": []}
 
 @app.route('/guardia', methods=['POST'])
 def cargar_guardia():
-    datos_guardia = request.get_json()
-    personas_guardia["guardias"].append(datos_guardia)
-    guardar_datos()
-    return jsonify({'message': 'Guardia cargada exitosamente'})
+    try:
+        datos_guardia = request.get_json()
+        personas_guardia["guardias"].append(datos_guardia)
+        guardar_datos()
+        return jsonify({'message': 'Guardia cargada exitosamente'})
+    except Exception as e:
+        return jsonify({'error': f'Error al cargar la guardia: {e}'})
 
 @app.route('/guardia/<int:dia>', methods=['GET'])
 def obtener_guardia(dia):
-    guardia = next((persona for persona in personas_guardia["guardias"] if persona['dia'] == dia), None)
-    if guardia:
-        return jsonify({'nombre': guardia['nombre'], 'chat_id': guardia['chat_id']})
-    else:
-        return jsonify({'error': 'No se encontró guardia para el día especificado'})
+    try:
+        guardia = next((persona for persona in personas_guardia["guardias"] if persona['dia'] == dia), None)
+        if guardia:
+            return jsonify({'nombre': guardia['nombre'], 'chat_id': guardia['chat_id']})
+        else:
+            return jsonify({'error': 'No se encontró guardia para el día especificado'})
+    except Exception as e:
+        return jsonify({'error': f'Error al obtener la guardia: {e}'})
 
 def guardar_datos():
-    with open('personas_guardia.json', 'w') as file:
-        json.dump(personas_guardia, file)
+    try:
+        with open(file_path, 'w') as file:
+            json.dump(personas_guardia, file)
+    except Exception as e:
+        print(f'Error al guardar los datos en el archivo JSON: {e}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
