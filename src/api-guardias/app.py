5dcd5179c5732e5ef89c6afdc4cb59904c06e4ad
Lautaro Torchia <torchia.lautaro2@gmail.com>
2023-06-28 22:54:30 -0300

    added api changes to make it work(needs refactoring still)

--- src/api-guardias/app.py
+++ src/api-guardias/app.py
@@ -1,10 +1,12 @@
 import os
 import json
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request,abort
+
 
 app = Flask(__name__)
 
 file_path = 'personas_guardia.json'
+dias_semana = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
 
 # Verificar si existe un archivo de datos guardados
 if os.path.isfile(file_path):
@@ -27,14 +29,24 @@ def cargar_guardia():
     except Exception as e:
         return jsonify({'error': f'Error al cargar la guardia: {e}'})
 
+
 @app.route('/guardia/<int:dia>', methods=['GET'])
 def obtener_guardia(dia):
     try:
-        guardia = next((persona for persona in personas_guardia["guardias"] if persona['dia'] == dia), None)
+        # Ensure the given dia is within the valid range
+        if dia < 1 or dia > 7:
+            return jsonify({'error': f'Los dias de la semana son entre el 1 y el 7:'}, 400)
+        
+        # Get the corresponding day of the week
+        target_day = dias_semana[dia - 1]
+        
+        # Find the guardia for the target day
+        guardia = next((persona for persona in personas_guardia["guardias"] if persona['dia'].lower() == target_day), None)
+        
         if guardia:
-            return jsonify({'nombre': guardia['nombre'], 'chat_id': guardia['chat_id']})
+            return jsonify({'nombre': guardia['nombre'], 'chat_id': guardia['chat_id'], 'telefono': guardia['telefono']})
         else:
-            return jsonify({'error': 'No se encontró guardia para el día especificado'})
+            return jsonify({'error': 'No se encontro guardia para el dia especificado'})
     except Exception as e:
         return jsonify({'error': f'Error al obtener la guardia: {e}'})
 
@@ -46,4 +58,4 @@ def guardar_datos():
         print(f'Error al guardar los datos en el archivo JSON: {e}')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000,host='0.0.0.0')
